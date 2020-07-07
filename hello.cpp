7bbc5fcfaa0e4f40c2054a45c57f7f2d7bfc287a
Lujialun <34935916+Lujialun@users.noreply.github.com>
2020-07-08 01:24:22 +0800

    update

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
- void FindNumbersWithSum(vector<int> array,int sum) {
+ vector<int> FindNumbersWithSum(vector<int> array,int sum) {
      vector<int> res;
      int left = 0, right = array.size()-1;
      while(left<right){
@@ -16,11 +16,46 @@ using namespace std;
              break;
          }
      }
-     cout << left << " " << right << endl;
+
+     int cur=left;
+     for(int i=0; i<=left&&i<cur; i++){
+        if(array[i]+array[cur]>sum){
+            //cout<<array[i]<<"here"<<array[cur]<<endl;
+            cur--;
+            i=-1;
+        }else if(array[i]+array[cur]<sum){
+            continue;
+        }else{
+
+            res.push_back(array[i]);
+            res.push_back(array[cur]);
+            return res;
+        }
+     }
+     return res;
+
+
 }
+ void FindNumsAppearOnce(vector<int> data,int* num1,int* num2) {
+        unordered_set<int> res;
+        for(int i=0; i<data.size(); i++){
+            auto it=res.find(data[i]);
+            if(it!=res.end()) res.erase(it);
+            else res.insert(data[i]);
+        }
+        int a=*(res.begin());
+        int b=*(++res.begin());
+    //    cout<<a<<" "<<b<<endl;
+        *num1=*res.begin();
+        *num2=*(res.begin()+1);
+    }
  int main()
  {
-    vector<int> a = {2, 4, 6, 8, 10};
-    FindNumbersWithSum(a, 10);
+    vector<int> a = {2,4,3,6,3,2,5,5};
+    int *c,*b;
+    FindNumsAppearOnce(a,c,b);
+
+    cout<<*c<<" "<<*b<<endl;
+
     system("pause");
  }
