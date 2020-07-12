b46eeefc179101c2dd625d7ba221e987ad2b7619
lujialun <939588042@qq.com>
2020-07-12 15:43:28 +0800

    update

---
+++ leetcode/leetcode174dp.cpp
@@ -0,0 +1,28 @@
+/*
+ * @Descripttion:  地下城游戏,救公主！ https://leetcode-cn.com/problems/dungeon-game/
+ * @version: 
+ * @Author: sueRimn
+ * @Date: 2020-07-12 14:16:10
+ * @LastEditors: sueRimn
+ * @LastEditTime: 2020-07-12 15:42:47
+ */ 
+#include<bits/stdc++.h>
+using namespace std;
+
+int calculateMinimumHP(vector<vector<int>>& dungeon) {
+    int n = dungeon.size(), m = dungeon[0].size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, INT_MAX));//初始化为最大值（最后一行比较时有用）
+    dp[n][m - 1] = dp[n - 1][m] = 1;//用于计算dp[n-1][m-1]
+    for (int i = n - 1; i >= 0;i--){
+        for (int j = m - 1; j >= 0; j--){
+            int min_path = min(dp[i + 1][j], dp[i][j + 1]);
+            dp[i][j] = max(min_path - dungeon[i][j], 1);
+        }
+    }
+    return dp[0][0];
+}
+int main()
+{
+    system("pause");
+    return 0;
+}
