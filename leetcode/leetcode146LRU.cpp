b11ee1aff705ce8cc54f4f001eebe90223227141
Lujialun <34935916+Lujialun@users.noreply.github.com>
2020-07-21 00:55:26 +0800

    udate

--- leetcode/leetcode146LRU.cpp
+++ leetcode/leetcode146LRU.cpp
@@ -4,32 +4,38 @@
  * @Author: sueRimn
  * @Date: 2020-07-20 19:24:16
  * @LastEditors: sueRimn
- * @LastEditTime: 2020-07-20 23:10:50
+ * @LastEditTime: 2020-07-21 00:54:43
  */ 
 #include <bits/stdc++.h>
 using namespace std;
 class LRUCache {
 public:
     LRUCache(int capacity) {
-        size=capacity;
+        cap=capacity;
     }
     
     int get(int key) {
-        int tmp = -1;
-        while(!cache.empty()){
-            if(key==cache.front().first){
-                
-            }
-        }
-        
-        
+        auto it = query.find(key);
+        if(it==query.end())
+            return -1;
+        int res = *(it->second);
+        cache.erase(it->second);
+         cache.push_back(res);
+        query[key] = cache.rbegin();
+       
+        return res;
     }
     
     void put(int key, int value) {
-
+        auto it = query.find(key);
+        if(it!=query.end()){
+            *(it->second) = value;
+            return;
+        }
+        if()
     }
 private:
-    int size;
-    queue<pair<int,int>> cache;
-    stack<int> temp;
+    int cap;
+    list<int> cache;
+    unordered_map<int, list<int>::reverse_iterator> query;
 };
