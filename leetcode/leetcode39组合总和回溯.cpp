a8e86f8bb9a7149b0e35c07e04ef31d72b1738ec
lujialun <939588042@qq.com>
2020-09-09 16:47:43 +0800

    update

---
+++ leetcode/leetcode39组合总和回溯.cpp
@@ -0,0 +1,40 @@
+/*
+ * @Descripttion:  https://leetcode-cn.com/problems/combination-sum/
+ * @version: 
+ * @Author: Lucas
+ * @Date: 2020-09-09 16:44:25
+ * @LastEditors: Lucas
+ * @LastEditTime: 2020-09-09 16:47:17
+ */
+class Solution {
+public:
+    void dfs(vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& combine, int idx) {
+        if (idx == candidates.size()) {
+            return;
+        }
+        if (target == 0) {
+            ans.emplace_back(combine);
+            return;
+        }
+        // 直接跳过
+        dfs(candidates, target, ans, combine, idx + 1);
+        // 选择当前数
+        if (target - candidates[idx] >= 0) {
+            combine.emplace_back(candidates[idx]);
+            dfs(candidates, target - candidates[idx], ans, combine, idx);
+            combine.pop_back();
+        }
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> combine;
+        dfs(candidates, target, ans, combine, 0);
+        return ans;
+    }
+};
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/combination-sum/solution/zu-he-zong-he-by-leetcode-solution/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
